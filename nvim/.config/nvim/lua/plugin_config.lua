c53da8aa34b01ea60888fa715b6d2b909fcd5534
Lucas Escobar <lucas.an.escobar@gmail.com>
2025-07-02 03:49:51 -0400

    Add nvim autocompletion for htmldjango files (used in zola)
    Signed-off-by: Lucas Escobar <lucas.an.escobar@gmail.com>

--- nvim/.config/nvim/lua/plugin_config.lua
+++ nvim/.config/nvim/lua/plugin_config.lua
@@ -152,6 +152,7 @@ conform.setup({
 		svelte = { "prettierd", "prettier", stop_after_first = true },
 		css = { "prettierd", "prettier", stop_after_first = true },
 		html = { "prettierd", "prettier", stop_after_first = true },
+		htmldjango = { "prettierd", "prettier", stop_after_first = true },
 		json = { "prettierd", "prettier", stop_after_first = true },
 		yaml = { "prettierd", "prettier", stop_after_first = true },
 		markdown = { "prettierd", "prettier", stop_after_first = true },
